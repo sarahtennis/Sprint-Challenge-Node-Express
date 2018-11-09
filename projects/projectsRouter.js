a3ff55049e3184fed6b6a96aef25858fccfedbea
sarahtennis <sarahtennis@Sarahs-Air.fios-router.home>
2018-11-09 12:25:19 -0500

    adds end points for /api/projects

--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -10,8 +10,69 @@ router.get('/:id', (req, res) => {
             res.status(200).json(project);
         })
         .catch(err => {
-            res.status(500).json({ errorMessage: 'Error retrieving project.' })
+            res.status(500).json({ errorMessage: 'Error retrieving project' });
+        });
+});
+
+// GET project actions by project id
+// ** Todo: add functionality to test if id exists
+router.get('/:id/actions', (req, res) => {
+    projectModel.getProjectActions(req.params.id)
+        .then(projectActions => {
+            res.status(200).json(projectActions);
         })
-})
+        .catch(err => {
+            res.status(500).json({ errorMessage: 'Error retrieving project actions' });
+        });
+});
+
+// POST insert new project
+router.post('/', (req, res) => {
+    if (req.body.name && req.body.description) {
+        projectModel.insert(req.body)
+            .then(newProject => {
+                res.status(201).json(newProject);
+            })
+            .catch(err => {
+                res.status(500).json({ errorMessage: 'Error creating project' })
+            })
+    } else {
+        res.status(400).json({ errorMessage: 'Must provide non-empty name and description' })
+    }
+});
+
+// DELETE remove post by post id
+router.delete('/:id', (req, res) => {
+    projectModel.remove(req.params.id)
+        .then(recordsDeleted => {
+            if (recordsDeleted) {
+                res.status(200).json({ successMessage: 'Successfully deleted project' });
+            } else {
+                res.status(404).json({ errorMessage: 'Project at specified id does not exist' });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ errorMessage: 'Error deleting project.' });
+        });
+});
+
+// PUT update post by post id
+router.put('/:id', (req, res) => {
+    if (req.body.name && req.body.description) {
+        projectModel.update(req.params.id, req.body)
+            .then(updatedProject => {
+                if (updatedProject) {
+                    res.status(200).json(updatedProject);
+                } else {
+                    res.status(404).json({ errorMessage: 'Project at specified id does not exist' });
+                }
+            })
+            .catch(err => {
+                res.status(500).json({ errorMessage: 'Error updating project' });
+            });
+    } else {
+        res.status(400).json({ errorMessage: 'Must provide non-empty name and description' })
+    }
+});
 
 module.exports = router;
